6ff132eefa079207e3a7d225598f9d3ce1b62479
Jo Asano Mac <69098381+Jo042@users.noreply.github.com>
2026-06-21 11:45:40 +0900

    chore: プロジェクトを初期化

---
+++ src/main/java/com/loopd/LoopdApplication.java
@@ -0,0 +1,13 @@
+package com.loopd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loopd/LoopdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
